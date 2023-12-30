5597bcf9be87e8357930414ae240968296d50a0e
Brunno Santana <brunnosantana98@gmail.com>
2023-12-29 21:49:34 -0300

    adicionando projeto message-store-service

---
+++ message-store-service/src/main/java/br/com/brunno/messagestoreservice/MessageStoreServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunno.messagestoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageStoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageStoreServiceApplication.class, args);
+	}
+
+}
---
+++ message-store-service/src/test/java/br/com/brunno/messagestoreservice/MessageStoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunno.messagestoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageStoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
